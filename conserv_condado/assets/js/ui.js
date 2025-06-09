fc36f89be9e4f8e535b3e0c4cebaf3d3281fb1f3
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-06-09 10:02:48 -0300

    feat: conserv ui

---
+++ conserv_condado/assets/js/ui.js
@@ -0,0 +1,24 @@
+const modal = document.getElementById('modal');
+const modalTitle = document.getElementById('modalTitle');
+const modalMessage = document.getElementById('modalMessage');
+const closeModalButton = document.getElementById('closeModal');
+let modalCallback = null;
+
+if (closeModalButton) {
+    closeModalButton.addEventListener('click', () => {
+        modal.classList.add('hidden');
+        if (modalCallback) {
+            modalCallback();
+            modalCallback = null;
+        }
+    });
+}
+
+function showModal(title, message, callback = null) {
+    if (modal && modalTitle && modalMessage) {
+        modalTitle.textContent = title;
+        modalMessage.textContent = message;
+        modalCallback = callback;
+        modal.classList.remove('hidden');
+    }
+}
